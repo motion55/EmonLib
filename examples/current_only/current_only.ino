db0d06d6a02aced4e936a6ec3493633290da91af
ButchTabios <butchtabios@yahoo.com>
2015-12-29 07:23:13 +0800

    modified current_only.ino to use new parameter

--- examples/current_only/current_only.ino
+++ examples/current_only/current_only.ino
@@ -7,12 +7,12 @@ void setup()
 {  
   Serial.begin(9600);
   
-  emon1.current(1, 111.1);             // Current: input pin, calibration.
+  emon1.current(1, 60.0);             // Current: input pin, calibration.
 }
 
 void loop()
 {
-  double Irms = emon1.calcIrms(1480);  // Calculate Irms only
+  double Irms = emon1.calcIrms(1000);  // Calculate Irms only
   
   Serial.print(Irms*230.0);	       // Apparent power
   Serial.print(" ");
