a06f3b8535a1a17b4d7dc7fe25dfc350ee33c26f
butchtabios <butchtabios@yahoo.com>
2016-01-06 11:56:13 +0800

    added blinking LED indicator

--- examples/interrupt_test/interrupt_test.ino
+++ examples/interrupt_test/interrupt_test.ino
@@ -8,16 +8,10 @@ const int ledPin =  13;      // the number of the LED pin
 // Variables will change :
 int ledState = LOW;             // ledState used to set the LED
 
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
-
-// constants won't change :
-const long interval = 1000;           // interval at which to blink (milliseconds)
-
 void setup() 
 {
   pinMode(ledPin, OUTPUT);
+
   
   Serial.begin(9600);
 
@@ -32,5 +26,17 @@ void loop()
   if (emon1.EnergyMeter())
   {
     emon1.serialprint();
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) 
+    {
+      ledState = HIGH;
+    }
+    else 
+    {
+      ledState = LOW;
+    }
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
   }
 }
